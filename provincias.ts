90bb6fbc1409c6c01c9d80b55a40d84f81b8cc28
alvarogr91 <38002465+alvarogr91@users.noreply.github.com>
2020-06-02 17:09:44 +0200

    Array con las provincias de España

---
+++ provincias.ts
@@ -0,0 +1,6 @@
+export const provinces = ['A Coruña/La Coruña','Alacant/Alicante','Albacete','Almería','Araba/Álava', 'Asturias','Ávila','Badajoz','Barcelona','Burgos','Cáceres',
+    'Bizkaia/Vizcaya','Cádiz','Cantabria','Castelló/Castellón','Ciudad Real','Córdoba','Cuenca','Girona/Gerona','Granada','Guadalajara',
+    'Gipuzkoa/Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lleida/Lérida','Lugo','Madrid','Málaga','Murcia','Navarra',
+    'Ourense/Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona',
+    'Santa Cruz de Tenerife','Teruel','Toledo','València/Valencia','Valladolid','Zamora','Zaragoza'
+]
