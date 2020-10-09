47272a5568e4768b7b79a2274753a9a334b95ccf
Álvaro González <alvarogr@pop-os>
2020-10-09 11:47:56 +0200

    Js con expresiones regulares útiles

---
+++ regex.js
@@ -0,0 +1,5 @@
+// DNI-NIE (credits to https://www.miguelangelmedina.es)
+let dniNieRegex = /((([X-Z])|([LM])){1}([-]?)((\d){7})([-]?)([A-Z]{1}))|((\d{8})([-]?)([A-Z]))/;
+
+// Email (credits to https://emailregex.com/)
+let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
